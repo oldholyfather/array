c98b07a04f3fdb9e9c5c0dcfb29eea24a02dabec
oldholyfather <113419458+oldholyfather@users.noreply.github.com>
2022-09-19 09:23:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[5];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(25);
+    Console.WriteLine(array[i]);
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    sum = array[i] + sum;
+}
+Console.WriteLine(sum);
+
